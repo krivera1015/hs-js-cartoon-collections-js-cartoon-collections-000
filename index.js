bb0f63036d99773d85b5e4348cb7591706294100
krivera1015 <krivera1015@gmail.com>
2018-07-03 00:13:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+  var numberedDwarves = []
+  for(var i = 0; i < dwarves.length; i++) {
+    numberedDwarves.push(`${i + 1}. ${dwarves[i]} `)
+  }
+  return numberedDwarves.join('')
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var newPlanet = [];
+  for(var i = 0; i < planeteerCalls.length; i++) {
+    newPlanet.push(`${planeteerCalls[i].toUpperCase()}!`);
+  }
+  return newPlanet;
 }
 
 function longPlaneteerCalls(words) {
+  for(var i =0; i < words.length; i++) {
+    if(words[i].length > 4) {
+      return true;
+    } else{
+      return false;
+    }
+  }
 }
 
 function findTheCheese (foods) {
+  for(var i =0; i < foods.length; i++) {
+    if(foods[i] === 'cheddar' || foods[i] === 'gouda' || foods[i] === 'camembert') {
+      return foods[i];
+    }
+  }
+  if(foods !== 'cheddar' || foods !== 'gouda' || foods !== 'camembert') {
+    return 'no cheese!';
+  }
 }
